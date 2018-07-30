2cbc4d414b0e569a5ceaa3701e35e50272e2f7be
JosCla <41910355+JosCla@users.noreply.github.com>
2018-07-30 09:31:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,23 +14,23 @@ for (var i = -50; i <= 50; i++) {
 setInterval(move, 1000/30);
 
 function move() {
-    // every 30th of a second movement will be calculated
-    
-    // first we undisplay the previous block
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillRect(posX + 48, 65 - (Math.pow(posX / 50, 2) / 2 * 25), 5, 5);
-    
-    // calculate new position
-    slopeAngle = Math.atan(posX / 50);
-    accX = grav * Math.sin(slopeAngle);
-    velX += accX;
-    posX += velX;
-    if (posX > 50) {posX -= 100;}
-    if (posX < -50) {posX += 100;}
-    
-    // draw new box
-    ctx.fillStyle = '#FF0000';
-    ctx.fillRect(posX + 49, 66 - (Math.pow(posX / 50, 2) / 2 * 25), 3, 3);
+	// every 30th of a second movement will be calculated
+	
+	// first we undisplay the previous block
+	ctx.fillStyle = '#FFFFFF';
+	ctx.fillRect(posX + 48, 65 - (Math.pow(posX / 50, 2) / 2 * 25), 5, 5);
+	
+	// calculate new position
+	slopeAngle = Math.atan(posX / 50);
+	accX = grav * Math.sin(slopeAngle);
+	velX += accX;
+	posX += velX;
+	if (posX > 50) {posX -= 100;}
+	if (posX < -50) {posX += 100;}
+	
+	// draw new box
+	ctx.fillStyle = '#FF0000';
+	ctx.fillRect(posX + 49, 66 - (Math.pow(posX / 50, 2) / 2 * 25), 3, 3);
 }
 
 function newGame() {
@@ -38,5 +38,3 @@ function newGame() {
 	velX = 0;
 	accX = 0;
 }
-
-console.log('task ACTUALLY complete');
