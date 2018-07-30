653f945acae09b60cb067a581f5bddc83ac65a90
JosCla <41910355+JosCla@users.noreply.github.com>
2018-07-30 09:33:54 -0600

    final
    this is hopefully the final update to this small gravity demonstration

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ newGame();
 var canvas = document.getElementById('graph');
 var ctx = canvas.getContext('2d');
 for (var i = -50; i <= 50; i++) {
-	ctx.fillRect(i + 50, 75 - (Math.pow(i / 50, 2) * 25), 1, 1);
+	ctx.fillRect(i + 50, 75 - (Math.pow(i / 50, 2) / 2 * 50), 1, 1);
 }
 
 setInterval(move, 1000/30);
@@ -18,7 +18,7 @@ function move() {
 	
 	// first we undisplay the previous block
 	ctx.fillStyle = '#FFFFFF';
-	ctx.fillRect(posX + 48, 60 - (Math.pow(posX / 50, 2) / 2 * 25), 5, 5);
+	ctx.fillRect(posX + 48, 65 - (Math.pow(posX / 50, 2) / 2 * 50), 5, 5);
 	
 	// calculate new position
 	slopeAngle = Math.atan(posX / 50);
@@ -30,7 +30,7 @@ function move() {
 	
 	// draw new box
 	ctx.fillStyle = '#FF0000';
-	ctx.fillRect(posX + 49, 61 - (Math.pow(posX / 50, 2) / 2 * 25), 3, 3);
+	ctx.fillRect(posX + 49, 66 - (Math.pow(posX / 50, 2) / 2 * 50), 3, 3);
 }
 
 function newGame() {
