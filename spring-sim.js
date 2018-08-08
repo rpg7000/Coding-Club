0831d72441de242b58d43f3557e0fa688c2a4a02
JosCla <41910355+JosCla@users.noreply.github.com>
2018-08-08 14:43:38 -0600

    more accurate physics
    now your spring will theoretically not defy the laws of physics.
    
    theoretically.

--- spring-sim.js
+++ spring-sim.js
@@ -1,4 +1,4 @@
-var posX, velX, accX, mass, isAttached, coefFrict, springConst, timer, onScreen;
+var posX, velX, accX, mass, isAttached, coefFrict, springConst, timer, multiplier;
 var canvas = document.getElementById("drawing");
 var ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#BBBBBB";
@@ -10,17 +10,6 @@ for (var i = 1; i < 40; i++) {
     
 }
 
-/*
-    In this model, 10 pixels represent one meter. All measurements are given in meters, meters/second, etc.
-    isAttached represents whether the spring and the block are attached. The spring is most stable at posX = 35m.
-    mass, posX, velX, and accX are all self-explanatory. posX is the right side of the block
-    
-    coefFrict represents the coefficient of friction of the floor.
-    
-    Though the spring returning to its original position could be modeled after launching the block, this will not
-    be incorporated.
-*/
-
 document.getElementById("btn-start").addEventListener("click", start);
 
 function move() {
@@ -28,22 +17,19 @@ function move() {
     ctx.clearRect(10, 0, 400, 210);
     ctx.clearRect(0, 190, 10, 20);
     
-    // checking if it has made contact with a spring
-    if (posX >= 35) {isAttached = true;}
-    if (posX <= 35) {isAttached = false;}
+    // checking if it has made contact with a spring, and whether to detach from the spring
+    if (posX > 35) {isAttached = true;}
+    if (posX < 35 && document.getElementById("attach").checked == false && isAttached == true) {isAttached = false;}
     
     // friction and spring acceleration
-    /* for friction, F = μN
-    N = m * 9.8, so F = μ * m * 9.8
-    acceleration of friction = F / m = (μ * m * 9.8) / m = μ * 9.8 */
-    /* for springs, F = -KX
-    acceleration of spring = F / m = -KX / m */
     accX = 0
     if (velX > 0) {accX = -9.8 * coefFrict;}
     if (velX < 0) {accX = 9.8 * coefFrict;}
     if (isAttached) {
         
-        accX += -(springConst * (posX - 35)) / mass;
+        // we need to many conditions to account for how multiplier will affect acceleration
+        if ((velX > 0 && posX > 35) || (velX <= 0 && posX < 35)) {accX += (-(springConst * (posX - 35)) / mass) * multiplier;}
+        if ((velX <= 0 && posX > 35) || (velX > 0 && posX < 35)) {accX += (-(springConst * (posX - 35)) / mass) / multiplier;}
         
     }
     
@@ -51,13 +37,12 @@ function move() {
     posX += velX / 60;
     velX += accX / 60;
     
-    // if it hits the right wall, stop it from going further, and stop it if its speed is too low (friction)
+    // if it hits the right wall, stop it from going further, and stop it if its speed is too low and not spring (friction)
     if (posX > 40) {posX = 40; velX = 0;}
-    if (Math.abs(velX) <= 9.8 * coefFrict / 60) {velX = 0;}
-    if (posX < 0) {onScreen = false;}
+    if ((Math.abs(velX) <= 9.8 * coefFrict / 60) && isAttached == false) {velX = 0;}
     
     // drawing the block
-    if (onScreen) {ctx.fillRect(posX * 10, 190, -20, 20);}
+    ctx.fillRect(posX * 10, 190, -20, 20);
     
     // drawing the spring
     if (isAttached) {
@@ -107,6 +92,14 @@ function start() {
     mass = parseFloat(document.getElementById("input-mass").value);
     coefFrict = parseFloat(document.getElementById("input-coeff").value);
     springConst = parseFloat(document.getElementById("input-const").value);
+    
+    /*
+        NOTE: Because of the method used to calculate motion (riemann sums) there is some error in movement calculation.
+        The multiplier variable accounts for this error in calculation, and is best optimized for Spring constant = mass * 100
+        I couldn't find a line of best fit that would accurately describe every ratio of springConst and mass.
+        Even if I would have found a line of best fit, it wouldn't work for every case, but this solution fixes most scenarios within reason.
+    */
+    multiplier = 1.127698522455 + ((springConst / mass) * 0.00170413510245);
     
     timer = setInterval(move, 1000/60);
     
